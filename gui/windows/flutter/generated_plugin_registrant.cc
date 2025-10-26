10b64a813ba1810995ddd26312cefe1f3463d748
fcavalcantirj <felipe.cavalcanti.rj@gmail.com>
2025-10-26 01:29:26 -0300

    Add Windows and Linux platform support for multi-platform builds

---
+++ gui/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_media_metadata/flutter_media_metadata_plugin.h>
+#include <media_kit_video/media_kit_video_plugin_c_api.h>
+#include <volume_controller/volume_controller_plugin_c_api.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FlutterMediaMetadataPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterMediaMetadataPlugin"));
+  MediaKitVideoPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("MediaKitVideoPluginCApi"));
+  VolumeControllerPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("VolumeControllerPluginCApi"));
+}
